9968177f2e090b78788faf4ddd6d734d4e6e0a68
YeeVonAngg <40588271+YeeVonAngg@users.noreply.github.com>
2018-08-27 23:23:14 +0800

    Update bubble_sort.cpp

--- C++/source/sorting/bubble_sort.cpp
+++ C++/source/sorting/bubble_sort.cpp
@@ -1,65 +1,67 @@
 /*
-    Bubble sort
-    -----------
-    A simple sorting algorithm that repeatedly compares pairs of adjacent
-    elements and swaps their positions if they are in the wrong order.
-    Also known as sinking sort.
-
-    Time complexity
-    ---------------
-    O(N^2), where N is the number of elements.
-
-    Space complexity
-    ----------------
-    O(1).
+Bubble sort
+-----------
+A simple sorting algorithm that repeatedly compares pairs of adjacent
+elements and swaps their positions if they are in the wrong order.
+Also known as sinking sort.
+Time complexity
+---------------
+O(N^2), where N is the number of elements.
+Space complexity
+----------------
+O(1).
 */
 
 #include <iostream>
 #include <vector>
 
-#include "sorting/utils.hpp"
-
 using namespace std;
 
-void bubble_sort(vector<int>& values, const int order = 1, const bool to_show_state = false) {
-    bool swapped;
-    size_t i, j;
-
-    for (i = 0; i < values.size() - 1; i++) {
-        swapped = false;
-        for (j = 0; i + j < values.size() - 1; j++) {
-            // 'order' is -1 for descending, so the inequality is reversed:
-            if (order * values[j] > order * values[j+1]) {
-                swap(values[j], values[j+1]);
-                swapped = true;
-
-                if (to_show_state)
-                    display_state(values);
-            }
-        }
-        if (!swapped)
-            break;
-    }
+template <typename T>
+bool bubble_sort(int size, const vector<T> &arr)
+{
+	bool isSorted = true;
+	int i, j;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap((int*)arr[j], (int*)arr[j + 1]);
+				isSorted = false;
+			}
+		}
+	}
+	return isSorted;
 }
 
-int main() {
-    size_t size;
-    get_input_size(size);
-
-    vector<int> values(size);
-    get_input_values(values, size);
-
-    int order;
-    string order_text;
-    get_order(order, order_text);
-
-    bool to_show_state;
-    get_whether_to_show_state(to_show_state);
-
-    bubble_sort(values, order, to_show_state);
-
-    cout << "\nThe values in " << order_text << " order are:\n";
-    display_state(values);
-
-    return 0;
+void swap(int* x, int* y)
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
+
+#ifndef BUBBLE_SORT_TEST
+int main()
+{
+	int size = 0;
+	cout << "Enter the input size : ";
+	cin >> size;
+
+	vector<int> arr(size);
+	cout << "Enter the numbers: " << endl;
+	for (int& num : arr)
+		cin >> num;
+
+	bubble_sort(size,arr);
+	cout << "The sorted numbers are: " << endl;
+	for (int index = 0; index < size; index++)
+		cout << arr[index] << ", ";
+
+	return 0;
+}
+
+#endif // BUBBLE_SORT_TEST
